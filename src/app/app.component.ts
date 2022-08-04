e0faf24c00a944d6e9e7d517206789840c02d672
VinhDang88 <vincedang720@gmail.com>
2022-08-04 19:28:04 -0400

    Correct Folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+  menu:MenuItem[] = [
+    {
+      item: "Chicken Fingers",
+      category: "Dinner",
+      price: 11.99
+    },
+    {
+      item: "Pizza",
+      category: "Dinner",
+      price: 15.99
+    },
+    {
+      item: "Wings",
+      category: "Sides",
+      price: 8.99
+    },
+    {
+      item: "Breadsticks",
+      category: "Sides",
+      price: 5.99
+    },
+    {
+      item: "Cesar Salad",
+      category: "Salads",
+      price: 5.99
+    },
+    {
+      item: "Hot Fudge Brownie",
+      category: "Desert",
+      price: 10.99
+    },
+
+
+
+
+  ]
+
+  names: string[] = ["Justin", "Josh", "Billy", "Tommy", "Jeff"];
 }
